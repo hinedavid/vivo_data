887ce1740a040e866e626fde7e4ab0e8697569e8
hinedavid <david@hine.co.cr>
2019-01-14 23:34:38 -0600

    Update report_details.blade.php

--- resources/views/lote/report_details.blade.php
+++ resources/views/lote/report_details.blade.php
@@ -28,35 +28,24 @@
 							<table class="table table-striped">
 								<thead>
 									<tr>
-										<th scope="col">#</th>
-										<th scope="col">Make</th>
-										<th scope="col">Model</th>
-										<th scope="col">Year</th>
-										<th scope="col">Cost</th>
+										<th scope="col">id</th>
+										<th scope="col">nombre del producto</th>
+										<th scope="col">fecha de entrega</th>
+										<th scope="col">miembro</th>
+										
 									</tr>
 								</thead>
 								<tbody>
+								@if (count($lote)>0)
+									@foreach($lote as $item)
 									<tr>
-										<th scope="row">1</th>
+										<th scope="row">{{$item->idlote}}</th>
 										<td>Mark</td>
 										<td>Otto</td>
 										<td>@mdo</td>
-										<td>500</td>
 									</tr>
-									<tr>
-										<th scope="row">2</th>
-										<td>Jacob</td>
-										<td>Thornton</td>
-										<td>@fat</td>
-										<td>500</td>
-									</tr>
-									<tr>
-										<th scope="row">3</th>
-										<td>Larry</td>
-										<td>the Bird</td>
-										<td>@twitter</td>
-										<td>500</td>
-									</tr>
+							     	@endforeach
+								@endif	
 								</tbody>
 							</table>
 						</div>
